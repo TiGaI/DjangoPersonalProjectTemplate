cd8351f3ffd2362eed1f98012ccb8db2545e87a8
Junjie Jiang <junjiejiang94@gmail.com>
2018-01-25 19:25:01 -0500

    addTestandAddstyle

--- polls/views.py
+++ polls/views.py
@@ -46,6 +46,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -56,12 +57,20 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+        		pub_date__lte = timezone.now()
+        	).order_by('-pub_date')[:5]
 
+# Question.objects.filter(pub_date__lte=timezone.now()) returns a queryset containing Questions whose pub_date is less than or equal to - that is, earlier than or equal to - timezone.now.
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+
+    	return Question.objects.filter(pub_date__lte= timezone.now())
 
 
 class ResultsView(generic.DetailView):
